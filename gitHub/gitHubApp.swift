683f99ebba779f10859ef9b09ad4e858235bcb3e
Denis Rudenko <dennyque.rudenko@gmail.com>
2023-09-17 19:22:01 -0700

    Initial Commit

---
+++ gitHub/gitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubApp.swift
+//  gitHub
+//
+//  Created by Denis Rudenko on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct gitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
